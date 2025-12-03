7bc83830e31d070e2c291a4138d15df0db3ef3b4
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-03 10:44:40 +0530

    Float variable

---
+++ src/Basics/Variable_Practice_Question_2.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class Variable_Practice_Question_2 {
+    static void main(String[] args) {
+        float var = 10.45f;
+        var = 20.55f;
+        System.out.println(var);
+    }
+}
