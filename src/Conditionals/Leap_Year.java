f1847d430296b0af818c7222fd3c57b56c284c9c
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-03 12:18:17 +0530

    This year is leap year or not.

---
+++ src/Conditionals/Leap_Year.java
@@ -0,0 +1,15 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class Leap_Year {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(year%4==0 && year!=100 || year==400){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+}
