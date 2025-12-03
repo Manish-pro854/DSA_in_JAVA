8a905ed90d44d24dd497633b8e15b7235f82c942
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-03 10:20:56 +0530

    Created folders

---
+++ src/Basics/Variable_Practice_Question_4.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Variable_Practice_Question_4 {
+    static void main(String[] args) {
+     //Find divisor and remainder
+        int a = 1042;
+        int b = 7;
+        int quo = a/b;
+        int rem = a%b;
+        System.out.println(quo + " " + rem);
+    }
+}
