5b2a97809965be3b1fff254f91fba5e49bc75c12
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-03 10:28:04 +0530

    Take input as string.

--- src/Basics/Agent_Binod.java
+++ src/Basics/Agent_Binod.java
@@ -1,4 +1,11 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class Agent_Binod {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String agentName = sc.nextLine();
+        System.out.println("Hi my name is agent "+agentName);
+    }
 }
