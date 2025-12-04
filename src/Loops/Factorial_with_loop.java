3664b01dcb8ee2f9abe95b6c1f7ffc0cda303d58
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-04 14:09:17 +0530

    Factorial with loop

---
+++ src/Loops/Factorial_with_loop.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class Factorial_with_loop {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int factorial = 1;
+        for(int i=1; i<=n; i++){
+            factorial = factorial*i;
+        }
+        System.out.println(factorial);
+    }
+}
