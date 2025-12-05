a30f7f8fe194f1b7b62a06675c09b160fbada67f
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-05 12:03:25 +0530

    Star Piramid

---
+++ src/Nested_Loops/Star_piramid.java
@@ -0,0 +1,25 @@
+package Nested_Loops;
+
+import java.util.Scanner;
+
+public class Star_piramid {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int number_of_lines = 1;
+        int stares = 1;
+        int spaces = n-1;
+        while(number_of_lines<=n){
+            for(int i=0; i<spaces; i++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<stares; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            spaces--;
+            stares++;
+            number_of_lines++;
+        }
+    }
+}
