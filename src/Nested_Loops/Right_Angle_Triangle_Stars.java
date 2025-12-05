c7afaaf162d3b0a24674293cfb65bd80c88cb58c
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-05 11:54:46 +0530

    Right Angle Triangle Stars

---
+++ src/Nested_Loops/Right_Angle_Triangle_Stars.java
@@ -0,0 +1,28 @@
+package Nested_Loops;
+
+import java.util.Scanner;
+
+public class Right_Angle_Triangle_Stars {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int curr_lines = 1;
+        int total_lines = n;
+        int stars = 1;
+        int spaces = n-1;
+        while(curr_lines<=total_lines) {
+            //print stars
+            for (int i = 0; i < stars; i++) {
+                System.out.print("* ");
+            }
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+            //Prepare for next line
+            System.out.println();
+            stars++;
+            spaces--;
+            curr_lines++;
+        }
+    }
+}
