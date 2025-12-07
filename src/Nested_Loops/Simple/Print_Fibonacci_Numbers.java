82ee05c9a358e76a984cad59e1d34463612ccfef
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-07 20:06:40 +0530

    Print n fibonacci number

--- src/Nested_Loops/Simple/Print_Fibonacci_Numbers.java
+++ src/Nested_Loops/Simple/Print_Fibonacci_Numbers.java
@@ -1,4 +1,20 @@
 package Nested_Loops.Simple;
 
+import java.util.Scanner;
+
 public class Print_Fibonacci_Numbers {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int first = 0;
+        int second = 1;
+        System.out.println(first);
+        System.out.println(second);
+        for(int i=2; i<n; i++){
+            int third = first+second;
+            System.out.println(third);
+            first = second;
+            second = third;
+        }
+    }
 }
