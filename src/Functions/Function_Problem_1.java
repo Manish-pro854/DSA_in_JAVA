396d1edf272a1b7d394446a99809aa319f66e50e
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-08 20:11:53 +0530

    Make function folder and also first program in function

---
+++ src/Functions/Function_Problem_1.java
@@ -0,0 +1,10 @@
+package Functions;
+
+public class Function_Problem_1 {
+    public static void fun(){
+        System.out.println("I am another function");
+    }
+    public static void main(String[] args) {
+        fun();
+    }
+}
