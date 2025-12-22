d974331eb74fea8decd370eb86060ba5263bf695
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-22 20:10:30 +0530

    Print_Character_Pattern

---
+++ src/Nested_Loops/Character_Pattern/Print_Character_Pattern.java
@@ -0,0 +1,25 @@
+package Nested_Loops.Character_Pattern;
+
+import java.util.Scanner;
+
+public class Print_Character_Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int number_of_lines = 1;
+        char ch = 'A';
+
+        while (number_of_lines <= n) {
+
+            // print character ch, number_of_lines times
+            for (int i = 1; i <= number_of_lines; i++) {
+                System.out.print(ch);
+            }
+
+            System.out.println();   // move to next line
+            ch++;                   // next character
+            number_of_lines++;      // next row
+        }
+    }
+}
