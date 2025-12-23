caa89f6e9576a60ada5cf78ac9ee868e5ff31e95
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-23 21:45:25 +0530

    Change int to double and vice-versa

---
+++ src/Java_Basics/Double_to_Int.java
@@ -0,0 +1,16 @@
+package Java_Basics;
+
+public class Double_to_Int {
+    static void main(String[] args) {
+        double a = 4.0;
+        double b = 1.0;
+        //change double to int
+        double ans = a*(1-(b/3)+(b/5)-(b/7)+(b/9)-(b/11));
+        System.out.println(ans);
+
+        //If we want to convert int to double so we do not need anything we do it smoothly but
+        // if we want to convert double into int then we do,
+        //double a = 10.0;
+        //int b = int a;
+    }
+}
