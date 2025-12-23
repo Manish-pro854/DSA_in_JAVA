c7c23ec655c69b291023fc4c2d3e27d33d0909cd
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-23 21:35:38 +0530

    Double gives more acurate result as except float

---
+++ src/Java_Basics/Expression_Evaluation.java
@@ -0,0 +1,14 @@
+package Java_Basics;
+
+import java.util.Scanner;
+
+public class Expression_Evaluation {
+    static void main(String[] args) {
+        double a = 25.5;
+        double b = 3.5;
+        double c = 40.5;
+        double d = 4.5;
+        double ans = ((a*b-b*b)/(c-d));
+        System.out.println(ans);
+    }
+}
