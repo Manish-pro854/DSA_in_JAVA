9195e106f5112c53bfb58024ecb4f67c75918f8e
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-24 09:12:57 +0530

    Divisible by 5 or either 11

---
+++ src/Java_Basics/Divisible_by_5_and_11.java
@@ -0,0 +1,17 @@
+package Java_Basics;
+
+import java.util.Scanner;
+
+public class Divisible_by_5_and_11 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n%5==0&&n%11==0){
+            System.out.println(n+" is divisible by 5 or either 11");
+        }else{
+            System.out.println(n+" is not divisible by either 5 or nor 11");
+        }
+        sc.close();
+    }
+}
