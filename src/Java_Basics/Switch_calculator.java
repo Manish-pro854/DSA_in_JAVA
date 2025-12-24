8c0b79224ac5debb9203f9c2093cc63c178c21a6
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-24 09:20:14 +0530

    Calculator using Switch

---
+++ src/Java_Basics/Switch_calculator.java
@@ -0,0 +1,40 @@
+package Java_Basics;
+
+import java.util.Scanner;
+
+public class Switch_calculator {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number a");
+        int a = sc.nextInt();
+        System.out.println("Enter number b");
+        int b = sc.nextInt();
+        System.out.println("Enter operator: +,-,*,/,%,");
+        char op = sc.next().charAt(0);
+
+        switch (op){
+            case '+':
+                System.out.println(a+b);
+                break;
+
+            case '-':
+                System.out.println(a-b);
+                break;
+
+            case '*':
+                System.out.println(a*b);
+                break;
+
+            case '/':
+                System.out.println(a/b);
+                break;
+
+            case '%':
+                System.out.println(a%b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+        sc.close();
+    }
+}
