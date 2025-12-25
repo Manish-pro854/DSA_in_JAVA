948dba0b0e159d050858f89d28415cf247b07e29
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-25 09:26:47 +0530

    Medium pattern 2 solved

--- src/Java_Basics/Nested_Loops_Practice/Medium_Pattern_2.java
+++ src/Java_Basics/Nested_Loops_Practice/Medium_Pattern_2.java
@@ -1,4 +1,21 @@
 package Java_Basics.Nested_Loops_Practice;
 
+import java.util.Scanner;
+
 public class Medium_Pattern_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int number_of_lines = 0;
+        int number = 1;
+        int i=1;
+        while(number_of_lines<n){
+            for(i=1; i<=number; i++){
+                System.out.print(number);
+            }
+            System.out.println();
+            number_of_lines++;
+            number++;
+        }
+    }
 }
