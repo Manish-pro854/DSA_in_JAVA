74393025b00a2ce414f3f77a8dfd18a8d922ea4f
Manish-pro854 <manishsoftengineer3@gmail.com>
2025-12-25 09:13:38 +0530

    Practised easy nested loops

---
+++ src/Java_Basics/Nested_Loops_Practice/Easy_Pattern.java
@@ -0,0 +1,23 @@
+package Java_Basics.Nested_Loops_Practice;
+
+import java.util.Scanner;
+
+public class Easy_Pattern {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int number_of_lines = 0;
+        int stars = 1;
+        while(number_of_lines<n){
+//            for(int i=1; i<=stars; i++){
+//                System.out.print("*");
+//            }
+            for(int i=1; i<=stars; i++){
+                System.out.print(i);
+            }
+            System.out.println();
+            number_of_lines++;
+            stars++;
+        }
+    }
+}
