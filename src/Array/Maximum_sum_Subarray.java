87b58618e7d944961f111f273b5df0861543f131
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-02 13:17:53 +0530

    Maximum Sum Subarray using kadane's algorithm and also find the the subaray starting and ending point

--- src/Array/Maximum_sum_Subarray.java
+++ src/Array/Maximum_sum_Subarray.java
@@ -7,14 +7,28 @@ public class Maximum_sum_Subarray {
         int max_sum = 0;
         int csum = 0;
 
+        // Answer starting, ending and current point
+        int asp = 0;
+        int aep = -1;
+        int csp = 0;
+
         for(int i=0; i< arr.length; i++){
             csum += arr[i];
 
             if(csum<0){
                 csum = 0;
+                csp = i+1;
             }
-            max_sum = Math.max(csum, max_sum);
+
+            if(max_sum<csum){
+                max_sum=csum;
+                //finding starting an ending point of the subarray
+                asp = csp;
+                aep = i;
+            }
         }
+        System.out.println(asp);
+        System.out.println(aep);
         return max_sum;
     }
     public static void main(String[] args) {
