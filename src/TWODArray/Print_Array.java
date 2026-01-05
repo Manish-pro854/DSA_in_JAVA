d6bbc807c47994e2d22c3a8a36f1648c5429f01b
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-05 22:09:27 +0530

    Print 2D Array

---
+++ src/TWODArray/Print_Array.java
@@ -0,0 +1,29 @@
+package TWODArray;
+
+import java.util.Scanner;
+
+public class Print_Array {
+    public static void print(int[][] arr){
+        int n = arr.length;
+        int m = arr[0].length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0; i<n; i++){
+             for(int j=0; j<m; j++){
+                 arr[i][j] = sc.nextInt();
+             }
+         }
+        print(arr);
+        sc.close();
+    }
+}
