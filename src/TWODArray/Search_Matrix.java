16cee574407fddb4b0c4b2e6e01647db8e318422
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-06 21:19:44 +0530

    Search the position of the element in the 2-D array

---
+++ src/TWODArray/Search_Matrix.java
@@ -0,0 +1,35 @@
+package TWODArray;
+
+import java.util.Scanner;
+
+public class Search_Matrix {
+    public static void Search(int[][] arr, int n, int m, int target){
+        int row = 0;
+        int col = m-1;
+
+        while(row<n && col>=0){
+            if(arr[row][col]<target){
+                row++;
+            } else if(arr[row][col]>target) {
+                col--;
+            }else{
+                System.out.println(row+" "+ col);
+                return;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int target = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        Search(arr,n,m,target);
+        sc.close();
+    }
+}
