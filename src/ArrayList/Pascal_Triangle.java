5ab1b5b93202b3c4ba344a1bc442d9a8f5c3e8b8
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-08 12:23:58 +0530

    Pascal triangle using ArrayList

---
+++ src/ArrayList/Pascal_Triangle.java
@@ -0,0 +1,45 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Pascal_Triangle {
+    public static ArrayList<ArrayList<Integer>> pascaltriangle(int numRows){
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+        ArrayList<Integer> past = new ArrayList<>();
+        past.add(1);
+        ans.add(past);
+
+        int total_number_of_rows = numRows-1;
+
+        while(total_number_of_rows>0){
+            ArrayList<Integer> curr = new ArrayList<>();
+
+            for(int i=1; i<past.size(); i++){
+                int csum = past.get(i)+ past.get(i-1);
+                curr.add(csum);
+            }
+            curr.add(0,1);
+            curr.add(1);
+
+//one way
+//            System.out.println(curr);
+
+            //second way
+//            ans.add(curr);
+
+            ArrayList<Integer> temp = new ArrayList<>(curr);
+            ans.add(temp);
+            past = curr;
+            total_number_of_rows--;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<ArrayList<Integer>> ans = pascaltriangle(n);
+        System.out.println(ans);
+    }
+}
