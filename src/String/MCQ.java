ac93b0695b0ece508afc9be640e8cce7334f963d
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-12 13:04:52 +0530

    Find which one score maximum number in MCQ

---
+++ src/String/MCQ.java
@@ -0,0 +1,43 @@
+package String;
+
+import java.util.Scanner;
+
+public class MCQ {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String anskey = sc.nextLine();
+        int n = anskey.length();
+
+        String minku = "ABC";
+        String ankit = "BABC";
+        String chinku = "CCAABB";
+
+        int minkuScore = 0;
+        int ankitScore = 0;
+        int chinkuScore = 0;
+
+        for(int i=0; i<n; i++){
+            char currAns = anskey.charAt(i);
+
+            if(currAns == minku.charAt(i%3)){
+                minkuScore++;
+            }
+            if(currAns == ankit.charAt(i%4)){
+                ankitScore++;
+            }
+            if(currAns == chinku.charAt(i%6)){
+                chinkuScore++;
+            }
+        }
+         int maxScore = Math.max(minkuScore,Math.max(ankitScore,chinkuScore));
+        if(maxScore==minkuScore){
+            System.out.println("Minku");
+        }
+        if(maxScore==ankitScore){
+            System.out.println("Ankit");
+        }
+        if(maxScore==chinkuScore){
+            System.out.println("Chinku");
+        }
+    }
+}
