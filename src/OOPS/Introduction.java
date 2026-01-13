cb84daf01cfa6dae9d9aa96619b5ff766e78cd71
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-13 18:10:55 +0530

    Learning starting with OOPS

---
+++ src/OOPS/Introduction.java
@@ -0,0 +1,34 @@
+package OOPS;
+
+class Student {
+    String name;
+    int roll_no;
+
+    public void makeNoise(){
+        System.out.println("Making Noise!!!!!");
+    }
+
+    public int sum(int a, int b){
+        return a+b;
+    }
+}
+public class Introduction {
+    static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "Accio";
+        s1.roll_no = 1;
+
+        Student s2 = new Student();
+
+        s2.name = "Ram";
+        s2.roll_no = 2;
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        s1.makeNoise();
+        System.out.println(s1.sum(5,5));
+    }
+}
