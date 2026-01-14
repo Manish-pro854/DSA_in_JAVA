efd826a973a189dd100907314b2af1b1ca836ff7
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-14 14:05:28 +0530

    Learn static keyword

--- src/OOPS/Static.java
+++ src/OOPS/Static.java
@@ -1,5 +1,7 @@
 package OOPS;
-
+//Non static methods channot change static variables
+//you can call static methods directly without creating object
+//Non-static functins can use static variables and also non satic variables
 class statics {
     String name;
     int age;
@@ -8,7 +10,14 @@ class statics {
     public statics(String name, int age){
         this.name = name;
         this.age = age;
+        this.total_students++;
     }
+
+    //only static methods use static variables
+    public static void printTotalStudent(){
+        System.out.println("Total Students are "+ total_students);
+    }
+
 }
 public class Static {
     static void main(String[] args) {
@@ -17,7 +26,17 @@ public class Static {
         statics s1 = new statics("A", 10);
         statics s2 = new statics("B", 20);
 
+        System.out.println(s1.total_students); //2
+
         System.out.println(s1.name);
         System.out.println(s2.name);
+
+        //static is same for everyone if one changed it then other also accessed the changed value
+        s1.total_students = 20;
+        System.out.println(s2.total_students);
+
+        statics s3 = new statics("E", 200);
+        s3.printTotalStudent();
+
     }
 }
