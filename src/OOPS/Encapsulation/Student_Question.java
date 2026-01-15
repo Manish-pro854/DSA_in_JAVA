23c15c74fa7158361592838feaf7a9c75fb7bc71
Manish-pro854 <manishsoftengineer3@gmail.com>
2026-01-15 13:40:51 +0530

    Get grade using private keyword

---
+++ src/OOPS/Encapsulation/Student_Question.java
@@ -0,0 +1,45 @@
+package OOPS.Encapsulation;
+
+class Student{
+    String name;
+    int roll_no;
+    private int marks;
+
+
+    public char getGrade(){
+        if(this.marks >= 85){
+            return 'A';
+        }else if(this.marks >= 60){
+            return 'B';
+        }else if(this.marks >= 45){
+            return 'C';
+        }else{
+            return 'D';
+        }
+    }
+
+    public void setMarks(int marks){
+        if(marks >100 || marks <0){
+            System.out.println("Not Possible");
+        }else{
+            this.marks = marks;
+        }
+    }
+
+    public int getMarks(int marks){
+        return this.marks;
+    }
+
+}
+
+public class Student_Question {
+    static void main(String[] args) {
+        Student s = new Student();
+
+        s.setMarks(85);
+        System.out.println(s.getGrade());
+
+        s.setMarks(45);
+        System.out.println(s.getGrade());
+    }
+}
